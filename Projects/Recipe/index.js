73fd6f5d5f6f9b2972493b181e6d17484ba3ec8a
courjimen <coureyfrance@gmail.com>
2025-01-24 18:14:38 -0500

    css style changes

--- Projects/Recipe/index.js
+++ Projects/Recipe/index.js
@@ -18,48 +18,50 @@ let label = document.createElement("label")
 label.htmlFor = "myCheckbox";
 
 label.appendChild(
-    document.createTextNode("Too spicy for you?")
+  document.createTextNode("Too spicy for you?")
 );
 
 toggle.appendChild(checkbox);
 toggle.appendChild(label);
 
 // adding strikethrough
-checkbox.addEventListener("click", function(){
-    if(checkbox.checked){
+checkbox.addEventListener("click", function () {
+  if (checkbox.checked) {
     label.classList = "checked";
-    } else {
-        label.classList.remove("checked");
-    }
+  } else {
+    label.classList.remove("checked");
+  }
 })
 
 // List checklist
+
+// grab every list item 
 let listItems = document.querySelectorAll("#ingredients li");
 
-// let liText = document.getElementById("test").innerHTML;
+// iterate through list and grab one list item
+listItems.forEach(ingredient => {
+  let checking = document.createElement("input");
+  checking.setAttribute("type", "checkbox");
+  ingredient.prepend(checking);
 
-// console.log(liText);
-
-listItems.forEach(e => {
-    let checking = document.createElement("input");
-    checking.setAttribute("type", "checkbox");
-    e.prepend(checking);
+  // check if box is checked
+  
 });
 
-checking.addEventListener("click", function(){
-    if(checking.checked){
-    liText.classList = "checked";
-    } else {
-        liText.classList.remove("checked");
-    }
-})
+
+// if box is checked, strikethrough label
+
+
+// adds checkboxes to ingredients
+
+
 
 function toggleIt() {
-    let x = document.getElementById("dropdown");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display ="none";
-    }
-   }
+  let x = document.getElementById("dropdown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
