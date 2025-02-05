f97c1a71b546d0d7c1691b0ee73a655770528d73
courjimen <coureyfrance@gmail.com>
2025-02-04 23:15:15 -0500

    added pseudo code for game js file

---
+++ Projects/HTML-Game/index.js
@@ -0,0 +1,4 @@
+// Make sure score is reading properly for heads & tails
+// add button functionality
+// call on the images not the flip function
+// write a function (if/else format upon on click)
