80e39747a07bf44955356a7f60b5b34b85f5f56c
courjimen <coureyfrance@gmail.com>
2025-02-04 23:57:46 -0500

    adds pseudocode

--- Projects/HTML-Game/index.js
+++ Projects/HTML-Game/index.js
@@ -1,4 +1,6 @@
-// Make sure score is reading properly for heads & tails
+// create a function that grabs the button (flip)
+    // upon click it flips the coin 
+    // call on the images not the flip function & write a function (if/else format upon on click)
+// Make sure score is properly counting the heads & tails
+
 // add button functionality
-// call on the images not the flip function
-// write a function (if/else format upon on click)
