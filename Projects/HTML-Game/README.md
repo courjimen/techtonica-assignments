de2cf1e7c039104b6a9b00bc3d96cf14dc051a19
courjimen <coureyfrance@gmail.com>
2025-02-05 23:12:22 -0500

    finally got my coin to flip and tallied head count

--- Projects/HTML-Game/README.md
+++ Projects/HTML-Game/README.md
@@ -1,3 +1,13 @@
 Coin Flip Game
 
 Flip the coin.
+
+# TODO
+- when the user clicks the button 
+    - get the image to change from heads to tails or vice versa
+- Add the randomness to the flip so it's 50% of the time heads and 50% of the time tails
+- Each time the user flips a coin, the result should be added to a tally -- heads or tails
+
+Stretch Goals
+- Flip animation 
+
--- Projects/HTML-Game/index.js
+++ Projects/HTML-Game/index.js
@@ -1,5 +1,33 @@
-// create a function that grabs the button (flip)
+//grab the button and store in a variable(flip)
+let flipCoinButton = document.getElementById("flip-button");
+let backCoin = document.getElementById("tails");
+let frontCoin = document.getElementById("heads");
+const headsScoreBox = document.getElementById("heads-score");
+
+let headsCount = 0;
+// console.log(flipCoinButton);
     // upon click it flips the coin 
+flipCoinButton.addEventListener("click", () => {
+    // if random === 1 heads
+    let random = Math.floor(Math.random() * (2 - 1 + 1) + 1);
+    if(random === 1){
+        headsCount += 1;
+        headsScoreBox.innerText = headsCount;
+    }
+    flip(random);
+})
+
+function flip (side) {
+    if(side === 1){
+        console.log("heads");
+        backCoin.style.visibility = "hidden";
+        frontCoin.style.visibility = "visible";
+    } else {
+        console.log("tails")
+        backCoin.style.visibility = "visible";
+        frontCoin.style.visibility = "hidden";
+    }
+}
     // call on the images not the flip function & write a function (if/else format upon on click)
 // Make sure score is properly counting the heads & tails
 
