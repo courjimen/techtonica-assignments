f94c0ee8d4ae6b7b1c2516d43d1d581e03d37359
courjimen <coureyfrance@gmail.com>
2025-02-09 15:31:16 -0500

    created coin game in react

---
+++ Projects/React-Game/Game/src/Counter.jsx
@@ -0,0 +1,10 @@
+const Counter = ({label, count}) => {
+    return (
+        <div>
+            <h3>{label}</h3>
+            <span>{count}</span>
+        </div>
+    )
+}
+
+export default Counter
