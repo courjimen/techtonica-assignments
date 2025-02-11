fd729858160149b2cf5e2e4381880b5ae526f9e4
courjimen <coureyfrance@gmail.com>
2025-02-11 00:05:28 -0500

    added 4 balloons

--- balloon-pop-game/src/components/Balloon.jsx
+++ balloon-pop-game/src/components/Balloon.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Balloon() {
   return (
-    <div>Balloon</div>
+    <>
+    <div>
+        <h3>Balloon</h3>
+    </div>
+    <div>
+        <h3>Balloon</h3>
+    </div>
+    <div>
+        <h3>Balloon</h3>
+    </div>
+    <div>
+        <h3>Balloon</h3>
+    </div>
+    </>
+
   )
 }
