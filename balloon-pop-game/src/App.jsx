e6797385e4d6f5fcf073958f8ffdf9c2864348b1
courjimen <coureyfrance@gmail.com>
2025-02-11 00:15:21 -0500

    linked balloon.jsx to app.jsx file

--- balloon-pop-game/src/App.jsx
+++ balloon-pop-game/src/App.jsx
@@ -5,6 +5,9 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  useState()
+  useState()
+  
   return (
     <>
       <Balloon />
--- balloon-pop-game/src/components/Balloon.jsx
+++ balloon-pop-game/src/components/Balloon.jsx
@@ -4,16 +4,16 @@ export default function Balloon() {
   return (
     <>
     <div>
-        <h3>Balloon</h3>
+        <h3>Balloon 1</h3>
     </div>
     <div>
-        <h3>Balloon</h3>
+        <h3>Balloon 2</h3>
     </div>
     <div>
-        <h3>Balloon</h3>
+        <h3>Balloon 3</h3>
     </div>
     <div>
-        <h3>Balloon</h3>
+        <h3>Balloon 4</h3>
     </div>
     </>
 
