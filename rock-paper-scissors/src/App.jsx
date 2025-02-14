38a9ed413eaceaf32911e1de8623df398b7d7893
courjimen <coureyfrance@gmail.com>
2025-02-13 22:18:06 -0500

    set props in player component

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,28 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import Player from './components/Player'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
+      <div className='center'>
 
-    <div className='center'>
+        <div>
+          <h1>ROCK PAPER SCISSORS</h1>
+        </div>
 
-      <div>
-      <h1>ROCK PAPER SCISSORS</h1>
+        <div className='container'>
+          <div className='player'><h2>Player: 0</h2></div>
+          <div className='player'><h2>Com: 0 </h2></div>
+        </div>
+
+        <div>
+          <button className='button'>Rock {count} </button>
+          <button>Paper {count} </button>
+          <button>Scissors {count} </button>
+        </div>
       </div>
 
-      <div className='container'>
-      <div className='player'><h2>Player: 0</h2></div>
-      <div className='player'><h2>Com: 0 </h2></div>
-      </div>
-
-      <div>
-      <button className='button'>Rock {count} </button>
-      <button>Paper {count} </button>
-      <button>Scissors {count} </button>
-      </div>
-    </div>
+      <Player 
+        name = "Player 1"
+        score = {0} />
+    </>
   )
 }
 
--- rock-paper-scissors/src/components/Player.jsx
+++ rock-paper-scissors/src/components/Player.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
-export default function Player() {
+export default function Player({name, score}) {
   return (
-    <div>Player</div>
+    <div>
+        <h3>{name="text"}</h3>
+        <h3>{score=Number}</h3>
+    </div>
   )
 }
