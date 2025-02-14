521ca2e12d22034d472b6fa8c3ea53d8e6d9ae14
courjimen <coureyfrance@gmail.com>
2025-02-14 01:12:23 -0500

    added useState for score counter

--- rock-paper-scissors/src/App.jsx
+++ rock-paper-scissors/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import {FaHandScissors, FaHandPaper, FaHandRock} from 'react-icons/fa' 
+import { FaHandScissors, FaHandPaper, FaHandRock } from 'react-icons/fa'
 
 const actions = {
   rock: "scissors",
@@ -16,47 +16,52 @@ function randomAction() {
 }
 
 function calculateWinner(action1, action2) {
-  if(action1 === action2) {
+  if (action1 === action2) {
     return 0;
+  } else if (actions[action1] === action2) {
+    return -1;
   }
+
+//This accounts for bugs in the code.
+  return null;
 }
 
-function ActionIcon({action, ...props}) {
+function ActionIcon({ action, ...props }) {
   const icons = {
-      rock: FaHandRock,
-      paper: FaHandPaper,
-      scissors: FaHandScissors
+    rock: FaHandRock,
+    paper: FaHandPaper,
+    scissors: FaHandScissors
   };
   const Icon = icons[action]
-return (
- <Icon {...props} />
-);
+  return (
+    <Icon {...props} />
+  );
 }
 
-function Player({ name = "Player", score = 0, action = "rock"}) {
+function Player({ name = "Player", score = 0, action = "rock" }) {
   return (
-      <div className='player'>
-          <h3 className='score'>{name} : {score}</h3>
-          <div className='action'>
-             {action  &&  <ActionIcon action={action} size={60} />}
-          </div>
+    <div className='player'>
+      <h3 className='score'>{name} : {score}</h3>
+      <div className='action'>
+        {action && <ActionIcon action={action} size={60} />}
       </div>
+    </div>
   )
 }
 
-function ActionButton ({action="rock", handlePlay}) {
+function ActionButton({ action = "rock", handlePlay }) {
   return (
     <button className='roundB' onClick={() => handlePlay(action)}>
-      <ActionIcon action={action} size={20}/>
-      </button>
+      <ActionIcon action={action} size={20} />
+    </button>
   )
 }
 function App() {
   const [playerAction, setPlayerAction] = useState("")
   const [comAction, setComAction] = useState("")
 
-const [playerScore, setPlayerScore] = useState(0);
-const [comScore, setComScore] = useState(0);
+  const [playerScore, setPlayerScore] = useState(0);
+  const [comScore, setComScore] = useState(0);
 
   const handlePlay = (playSelected) => {
     setPlayerAction(playSelected)
@@ -64,6 +69,12 @@ const [comScore, setComScore] = useState(0);
 
     setPlayerAction(playSelected);
     setComAction(newComAction);
+    const winner = calculateWinner(playSelected, newComAction);
+    if (winner === -1) {
+      setPlayerScore(playerScore + 1);
+    } else if (winner === 1) {
+      setComScore(comScore + 1);
+    }
   };
 
   return (
@@ -79,20 +90,20 @@ const [comScore, setComScore] = useState(0);
           <Player
             name="Player 1"
             score={playerScore}
-            action= {playerAction}/>
+            action={playerAction} />
 
           <Player
             name="Com"
-            score={comScore} 
+            score={comScore}
             action={comAction} />
         </div>
 
-       
+
 
         <div>
-          <ActionButton action="rock" handlePlay={handlePlay}/>
+          <ActionButton action="rock" handlePlay={handlePlay} />
           <ActionButton action="paper" handlePlay={handlePlay} />
-          <ActionButton action="scissors" handlePlay={handlePlay}/>
+          <ActionButton action="scissors" handlePlay={handlePlay} />
         </div>
         <h2> Player 1 Wins</h2>
       </div>
