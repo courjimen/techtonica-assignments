31d19ea2a8c18cdcf7329d1c98217c4103a227be
courjimen <coureyfrance@gmail.com>
2025-03-28 12:21:32 -0400

    created player name component

--- rock-paper-scissors/client/src/components/PlayerForm.jsx
+++ rock-paper-scissors/client/src/components/PlayerForm.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { use, useState } from 'react'
 
-function PlayerForm() {
+function PlayerForm({ newPlayer }) {
+    const [playerName, setPlayerName] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        newPlayer(playerName)
+    }
+
   return (
-    <div>PlayerForm</div>
+    <div className='player-form'>
+        <h2>Enter your name</h2>
+        <form onSubmit={handleSubmit}> 
+            <input 
+            type="text"
+            value={playerName}
+            placeholder='Your name'
+            onChange={(e) => setPlayerName(e.target.value)}
+            />
+            <button type='submit'>Start Game</button>
+        </form>
+    </div>
   )
 }
 
